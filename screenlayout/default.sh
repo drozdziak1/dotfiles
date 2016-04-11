0e718e5eb35f3157d38c52bbff6a9fb23d3fe34d
Stanisław Drozd <drozdziak1@gmail.com>
2016-04-11 18:00:00 +0200

    switch monitor connectors

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1280x1024 --pos 0x688 --rotate normal --output DVI-I-2 --mode 1920x1080 --pos 1280x0 --rotate left
+xrandr --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1920x1080 --pos 1280x0 --rotate left --output DVI-I-2 --mode 1280x1024 --pos 0x648 --rotate normal
