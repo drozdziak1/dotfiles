1922873fcea5e488814c635c4df611505b9650a3
Stanisław Drozd <drozdziak1@gmail.com>
2016-09-04 18:59:08 +0200

    comment second screen workspace reservation out, update oh-my-zsh, update screen layout

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --mode 1280x1024 --pos 0x632 --rotate normal --output DVI-I-2 --mode 1920x1080 --pos 1280x0 --rotate left
+xrandr --output DVI-I-1 --off --output DVI-I-2 --mode 1920x1080 --pos 0x0 --rotate normal
