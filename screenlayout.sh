c21e3df515f176c52ed79edf56fb398a82a4e100
Stanisław Drozd <drozdziak1@gmail.com>
2016-09-05 13:52:53 +0200

    facilitate work branch for new workspace

--- screenlayout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI1 --off --output VGA-1-2 --off --output LVDS1 --primary --mode 1366x768 --pos 0x456 --rotate normal --output VGA1 --mode 1920x1080 --pos 1366x0 --rotate normal
